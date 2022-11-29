c2af7eee65f1844fa54f4399ef9b37f94777aad2
CristovamOliver <contato.lucas55@gmail.com>
2022-11-29 19:13:46 -0300

    Adição de controllers, e contexto de banco

---
+++ API.Veiculo/Infrastructure/Data/Context.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Data
+{
+
+    public class Context : IDisposable
+    {
+        private readonly IConfiguration _configuration;
+        public Context(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        public string GetConnection()
+        {
+
+            {
+                var connection = _configuration.GetConnectionString("DefaultConnection");
+                return connection;
+            }
+
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
