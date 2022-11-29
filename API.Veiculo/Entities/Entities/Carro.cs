4dea5b40dcad88b1833edd3edb30c16970583e2f
CristovamOliver <contato.lucas55@gmail.com>
2022-11-28 21:59:27 -0300

    "Estruturando projeto para o DDD"

---
+++ API.Veiculo/Entities/Entities/Carro.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Entities.Entities
+{
+    public class Carro
+    {
+        public Carro(int carroID, string modelo, int ano, string cor, int kM, Usuario usuarioID)
+        {
+            CarroID = carroID;
+            Modelo = modelo;
+            Ano = ano;
+            Cor = cor;
+            KM = kM;
+            UsuarioID = usuarioID;
+        }
+    
+        public int CarroID { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public string Cor { get; set; }
+        public int KM { get; set; }
+        public Usuario UsuarioID { get; set; }
+    }
+}
---
+++ API.Veiculo/Entities/Entities/Marca.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Entities.Entities
+{
+    public class Marca
+    {
+        public int MarcaID { get; set; }
+        public string NomeMarca { get; set; }
+        public Carro carroID { get; set; }
+    }
+}
